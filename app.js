ce8851e4fe5726567df2be138d2da7b1427f3e0b
damiodev <oweb.digital@gmail.com>
2023-06-18 19:19:05 +0200

    Retrieving the list of objects

--- app.js
+++ app.js
@@ -32,25 +32,9 @@ app.post('/api/stuff', (req, res, next) => {
 });
 
 app.use('/api/stuff', (req, res, next) => {
-    const stuff = [
-        {
-            _id: 'oeihfzeoi',
-            title: 'Mon premier objet',
-            description: 'Les infos de mon premier objet',
-            imageUrl: 'https://cdn.pixabay.com/photo/2014/09/12/12/04/eyeglasses-442616_1280.jpg',
-            price: 4900,
-            userId: 'qsomihvqios',
-        },
-        {
-            _id: 'oeihfzeomoihi',
-            title: 'Mon deuxième objet',
-            description: 'Les infos de mon deuxième objet',
-            imageUrl: 'https://cdn.pixabay.com/photo/2015/11/19/21/11/atlas-1052011_1280.jpg',
-            price: 2900,
-            userId: 'qsomihvqios',
-        },
-    ];
-    res.status(200).json(stuff);
+    Thing.find()
+        .then(things => res.status(200).json(things))
+        .catch(error => res.status(400).json({ error }));
 });
 
 module.exports = app;
